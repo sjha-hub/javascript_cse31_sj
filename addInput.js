795185f9c96f799d332cda2c481d89b87952150b
Samiksha <samikshaajha@gmail.com>
2026-08-03 11:35:24 +0530

    remove unwanted file

--- addInput.js
+++ addInput.js
@@ -1,5 +1,12 @@
-let a=prompt("Enter you number");
-let b=prompt("enter your 2 no.");
-let c=Number(a)+Number(b);
-//console.log("sum=",c)
-document.write("Sum=",c )
+let a = prompt("Enter your first number:");
+let b = prompt("Enter your second number:");
+
+let c = Number(a) + Number(b);
+let d = Number(a) - Number(b);
+let e = Number(a) * Number(b);
+let f = Number(a) / Number(b);
+
+document.write("Sum = " + c + "<br>");
+document.write("Subtraction = " + d + "<br>");
+document.write("Multiplication = " + e + "<br>");
+document.write("Division = " + f + "<br>");

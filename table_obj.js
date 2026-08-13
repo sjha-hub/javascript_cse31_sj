2de6a957635677d1e3daea555c81dcceb15aaf9e
Samiksha <samikshaajha@gmail.com>
2026-08-13 13:54:00 +0530

    Table created in terminal

---
+++ table_obj.js
@@ -0,0 +1,19 @@
+let student = [
+    {
+        name: "Samyara",
+        city: "Delhi",
+        Cgpa: 9.8           
+    },                       //  Comma between objects
+    {
+        name: "papaya",
+        city: "Delhi",
+        Cgpa: 7.8
+    },                       //  Comma between objects
+    {
+        name: "samiksha",
+        city: "Delhi",
+        Cgpa: 8.8
+    }                        
+];
+
+console.table(student);

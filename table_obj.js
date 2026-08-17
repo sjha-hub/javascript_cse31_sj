7e5886c351dbfc333cb44ae86af999e9fee7a44b
Samiksha <samikshaajha@gmail.com>
2026-08-17 11:12:02 +0530

    table created

--- table_obj.js
+++ table_obj.js
@@ -6,14 +6,32 @@ let student = [
     },                       //  Comma between objects
     {
         name: "papaya",
-        city: "Delhi",
-        Cgpa: 7.8
+        city: "crossing republic",
+        Cgpa: 10.0
     },                       //  Comma between objects
     {
         name: "samiksha",
         city: "Delhi",
-        Cgpa: 8.8
-    }                        
+        Cgpa: 7.8
+    },
+     {
+        name: "jha",
+        city: "Delhi",
+        Cgpa: 7.8
+    }  ,
+     {
+        name: "dixit",
+        city: "Delhi",
+        Cgpa: 7.8
+    }   
+
 ];
 
 console.table(student);
+for (let i =0; i<student.length; i++){
+    if (student[i].Cgpa>=8){
+        console.log(student[i]);
+    }
+}
+
+console.log('the cost of ${obj.name} is ${obj.price}');

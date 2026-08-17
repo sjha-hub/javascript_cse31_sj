844ee2281673f9faed98e8a447953de86046c42b
Samiksha <samikshaajha@gmail.com>
2026-08-17 11:05:09 +0530

    string length

---
+++ string.js
@@ -0,0 +1,19 @@
+let a = "wassup ppl"
+let count1 = 0
+//1. 
+for (let i=0; i<a.length; i++){
+    count1++
+}
+
+console.log("length of the array is", count1)
+
+//2.
+let count=0
+while(a[count]!==undefined){
+    count++
+}
+
+console.log("length of the array is", count)
+
+//3. 
+console.log("length of the array is", a.length)

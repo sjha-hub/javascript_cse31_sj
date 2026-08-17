30ee5ca7494b588c0c753e27c90e1a14e77b54e7
Samiksha <samikshaajha@gmail.com>
2026-08-17 11:10:25 +0530

    string length

--- string.js
+++ string.js
@@ -5,15 +5,15 @@ for (let i=0; i<a.length; i++){
     count1++
 }
 
-console.log("length of the array is", count1)
+console.log("length of the array displayed using for loop", count1)
 
 //2.
 let count=0
-while(a[count]!==undefined){
+while(a[count]!=undefined){
     count++
 }
 
-console.log("length of the array is", count)
+console.log("length of the array displayed using while loop terminates at undefined", count)
 
 //3. 
-console.log("length of the array is", a.length)
+console.log("length of the array display using pre-defined function", a.length)
